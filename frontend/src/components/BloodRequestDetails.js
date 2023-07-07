5eaa8c64a5a14be435dc680d09a72da9cd7f8199
Guren25 <felixjuaton87@gmail.com>
2023-07-07 09:19:53 +0800

    CSS ADDED

--- frontend/src/components/BloodRequestDetails.js
+++ frontend/src/components/BloodRequestDetails.js
@@ -30,7 +30,7 @@ const BloodRequestDetails = ({ bloodRequest }) => {
     <div className="blood-request-details">
       <h4>{bloodRequest.patientName}</h4>
       <p><strong>Blood Type: </strong>{bloodRequest.bloodType}</p>
-      <p><strong>location: </strong>{bloodRequest.location}</p>
+      <p><strong>Location: </strong>{bloodRequest.location}</p>
       <p><strong>Phone Number: </strong>{bloodRequest.phoneNumber}</p>
       <p>{formatDistanceToNow(new Date(bloodRequest.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
