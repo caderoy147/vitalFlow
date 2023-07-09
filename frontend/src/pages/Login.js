5e05d565b0e6d38a973021e994e2af9f8a75f4c6
caderoy147 <benedictjamesenj@gmail.com>
2023-07-09 18:11:02 +0800

    remove eslint warning

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 
-
-
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,45 +12,58 @@ const Login = () => {
     await login(email, password);
   };
 
+  const handleForgotPassword = () => {
+    // Handle the forgot password functionality
+  };
+
   return (
-    
-      <form className="login" onSubmit={handleSubmit}>
-    <div className="image-wrapper">
-      <img src="./images/vitalflow.png" alt="VITAL LOGO" className="img1" />
-    </div>
-    <h3 className="LUG">LOG IN TO YOUR ACCOUNT</h3>
-    <div className="form-wrapper">
-      <label></label>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-
-      <label></label>
-      <div className="password-wrapper">
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <a href="#" className="forgot-password">Forgot Password?</a>
+    <form className="login" onSubmit={handleSubmit}>
+      <div className="image-wrapper">
+        <img src="./images/vitalflow.png" alt="VITAL LOGO" className="img1" />
       </div>
+      <h3 className="LUG">LOG IN TO YOUR ACCOUNT</h3>
+      <div className="form-wrapper">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
 
-      <button disabled={isLoading}>Sign in</button>
-      {error && <div className="error">{error}</div>}
+        <label htmlFor="password">Password</label>
+        <div className="password-wrapper">
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <button
+            type="button"
+            className="forgot-password"
+            onClick={handleForgotPassword}
+          >
+            Forgot Password?
+          </button>
+        </div>
 
-        <p className = "OR"> OR </p>
-        <p className = "Signwith"> Sign in with </p>
-      <div className="image-wrapper2">
-      <img href = "" src="./images/google.png" alt="Google" className="img2" />
-      <img href = "" src="./images/ms.png" alt="MS" className="img3" />
-    </div>
-    </div>
-    <p className="Signwith"> Don't have an account? <a href="/signup" className="noAcc"> Sign up</a></p>
-  </form>
+        <button disabled={isLoading}>Sign in</button>
+        {error && <div className="error">{error}</div>}
+
+        <p className="OR">OR</p>
+        <p className="Signwith">Sign in with</p>
+        <div className="image-wrapper2">
+          <img src="./images/google.png" alt="Google" className="img2" />
+          <img src="./images/ms.png" alt="MS" className="img3" />
+        </div>
+      </div>
+      <p className="Signwith">
+        Don't have an account? <a href="/signup" className="noAcc">Sign up</a>
+      </p>
+    </form>
   );
 };
 
